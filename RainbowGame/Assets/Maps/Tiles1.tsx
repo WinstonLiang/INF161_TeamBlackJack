bbbbe982bcd8992addbe62f31ca661a4fb1d3b9a
Winston Liang <alakamande@gmail.com>
2017-10-26 12:47:09 -0700

    all of tiled2unity

---
+++ RainbowGame/Assets/Maps/Tiles1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tiles" tilewidth="48" tileheight="48" tilecount="8" columns="2">
+ <image source="Tiles.png" width="96" height="192"/>
+</tileset>
