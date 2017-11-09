91db7f4051b79a868843777d54a623f4b2ee4103
Winston Liang <alakamande@gmail.com>
2017-11-09 13:28:12 -0800

    a test map with coin placements

---
+++ RainbowGame/Assets/Maps/Coin.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Coin" tilewidth="48" tileheight="48" tilecount="1" columns="1">
+ <image source="../../../Coin.png" width="48" height="48"/>
+</tileset>
