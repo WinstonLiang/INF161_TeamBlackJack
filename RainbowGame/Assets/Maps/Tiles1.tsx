acb697ffb6294b618b95fca235d0ff0bb1911ee1
Winston Liang <alakamande@gmail.com>
2017-11-16 13:09:22 -0800

    black borders

--- RainbowGame/Assets/Maps/Tiles1.tsx
+++ RainbowGame/Assets/Maps/Tiles1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Tiles" tilewidth="48" tileheight="48" tilecount="8" columns="2">
- <image source="Tiles.png" width="96" height="192"/>
+<tileset name="Tiles1" tilewidth="48" tileheight="48" tilecount="8" columns="2">
+ <image source="../../../Tiles.png" width="96" height="192"/>
 </tileset>
