0362093208c781a934bc1764a5265db62e35daa3
Noel Biscocho <biscochn@uci.edu>
2017-11-29 23:11:08 -0800

    Adding a title scene and functionality to load into the game with different controls. Added Title Scene and Lucas' test scene to build settings to be loaded by scripts.

---
+++ RainbowGame/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadGame : MonoBehaviour {
+
+    private Button keyboard;
+    private Button mouse;
+    public int choice;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        keyboard = GameObject.Find("Keyboard").GetComponent<Button>();
+        mouse = GameObject.Find("Mouse").GetComponent<Button>();
+        keyboard.onClick.AddListener(LoadKeyboard);
+        mouse.onClick.AddListener(LoadMouse);
+    }
+
+    private void LoadKeyboard()
+    {
+        choice = 1;
+        SceneManager.LoadScene("LAmutan");
+    }
+
+    private void LoadMouse()
+    {
+        choice = 2;
+        SceneManager.LoadScene("LAmutan");
+    }
+
+}
---
+++ RainbowGame/Assets/Scripts/SelectScheme.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectScheme : MonoBehaviour {
+
+    private void Awake()
+    {
+        if (!GameObject.Find("Settings"))
+        {
+            GameObject.Find("testPlayer").GetComponent<ChaseMouse>().enabled = false;
+        }
+        else if(GameObject.Find("Settings").GetComponent<LoadGame>().choice == 1)
+        {
+            GameObject.Find("testPlayer").GetComponent<ChaseMouse>().enabled = false;
+        }
+        else if(GameObject.Find("Settings").GetComponent<LoadGame>().choice == 2)
+        {
+            GameObject.Find("testPlayer").GetComponent<Movement>().enabled = false;
+        }
+    }
+
+
+}
