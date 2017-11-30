a6d63fa1806b291354c45408eeb8232fa39d730b
jdicamillo <jdicamil@uci.edu>
2017-11-29 20:46:35 -0800

    Matches the behavior of the Movement script.

--- RainbowGame/Assets/Scripts/ChaseMouse.cs
+++ RainbowGame/Assets/Scripts/ChaseMouse.cs
@@ -5,44 +5,62 @@ using UnityEngine;
 
 public class ChaseMouse : MonoBehaviour {
 
-    private Vector3 movement = Vector3.zero;
     private Rigidbody rigidBody;
     private bool jump = false;
     private float distToGround;
+    private SoundManager sound;
+    private float diffX, diffY;
 
-    public float speed = 18.0F;
-    public float jumpForce = 5.0F;
-    public float controllerTolerance = 0.01f;
+    public float thrust = 0.25f;
+    public float jumpForce = 5.75f;
+    public float controllerTolerance = 5.00f;
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         distToGround = GetComponent<Collider>().bounds.extents.y;
         rigidBody = GetComponent<Rigidbody>();
+        sound = GameObject.Find("AudioPlayer").GetComponent<SoundManager>();
     }
-	
+
     void Update()
     {
         var playerCoords = Camera.main.WorldToViewportPoint(transform.position);
         var mouseCoords = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-        var diffX = mouseCoords.x - playerCoords.x;
-        var diffY = mouseCoords.y - playerCoords.y;
-        if (Math.Abs(diffX) > controllerTolerance)
-            movement.x = diffX > 0.0f ? 1.0f : -1.0f;
-        if (Math.Abs(diffY) > controllerTolerance)
-            movement.z = diffY > 0.0f ? 1.0f : -1.0f;
-        movement *= speed * Time.deltaTime;
+        diffX = mouseCoords.x - playerCoords.x;
+        diffY = mouseCoords.y - playerCoords.y;
         jump = Input.GetMouseButtonDown(0);
+        thrust = (jump && isGrounded()) ? 0.25f : 0.5f;
     }
 
     void FixedUpdate()
     {
         if (jump && isGrounded())
-            rigidBody.AddForce(new Vector3(0.0f, jumpForce, 0.0f), ForceMode.Impulse);
-        rigidBody.MovePosition(transform.position + movement);
+            playerJump();
+        if (Math.Abs(diffX) > controllerTolerance )
+        {
+            if (diffX > 0 && rigidBody.velocity.x < 5)
+                rigidBody.AddForce(new Vector3(thrust, 0, 0), ForceMode.Impulse); 
+            else if (diffX < 0 && rigidBody.velocity.x > -5)
+                rigidBody.AddForce(new Vector3(-thrust, 0, 0), ForceMode.Impulse);
+        }
+        if (Math.Abs(diffY) > controllerTolerance)
+        {
+            if (diffY > 0 && rigidBody.velocity.z < 5)
+                rigidBody.AddForce(new Vector3(0, 0, thrust), ForceMode.Impulse);
+            else if (diffY < 0 && rigidBody.velocity.z > -5)
+                rigidBody.AddForce(new Vector3(0, 0, -thrust), ForceMode.Impulse);
+        }
     }
 
     private bool isGrounded()
     {
         return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+
+    private void playerJump()
+    {
+        sound.PlayJumpSound();
+        rigidBody.AddForce(new Vector3(0.0f, jumpForce, 0.0f), ForceMode.VelocityChange);
     }
 }
